edc315b5a3885b133f98a9f4aeb3a72632353dc0
sertanozcnn <sertanozcan.tr@gmail.com>
2022-11-09 17:35:19 +0300

    İnternet Programlama

--- Django Project/appSale/admin.py
+++ Django Project/appSale/admin.py
@@ -2,5 +2,7 @@ from django.contrib import admin
 
 # Register your models here.
 from django.contrib import admin
-from .models import Movie
+from .models import Movie,fiyatlandirma
+
 admin.site.register(Movie)
+admin.site.register(fiyatlandirma)
--- Django Project/appSale/migrations/0001_initial.py
+++ Django Project/appSale/migrations/0001_initial.py
@@ -17,8 +17,21 @@ class Migration(migrations.Migration):
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('title', models.CharField(max_length=100)),
                 ('description', models.CharField(max_length=250)),
-                ('image', models.ImageField(upload_to='movie/images/')),
-                ('url', models.URLField(blank=True)),
+                # ('image', models.ImageField(upload_to='movie/images/')),
+                # ('url', models.URLField(blank=True)),
             ],
         ),
+        
+         migrations.CreateModel(
+            name='fiyatlandirma',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('title', models.CharField(max_length=100)),
+                ('description', models.CharField(max_length=250)),
+                # ('image', models.ImageField(upload_to='movie/images/')),
+                # ('url', models.URLField(blank=True)),
+            ],
+        ),
+        
+        
     ]
